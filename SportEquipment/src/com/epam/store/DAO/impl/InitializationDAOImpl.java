da7876407826180004ed8471e97c79acc36bf11b
Archidel <archangel.albert.6@gmail.com>
2016-12-13 20:25:55 +0300

    class initdao

--- SportEquipment/src/com/epam/store/DAO/impl/InitializationDAOImpl.java
+++ SportEquipment/src/com/epam/store/DAO/impl/InitializationDAOImpl.java
@@ -1,14 +1,27 @@
 package com.epam.store.DAO.impl;
 
+import java.sql.SQLException;
+
 import com.epam.store.DAO.InitializationDAO;
+import com.epam.store.DAO.connection.ConnectionPool;
 import com.epam.store.bean.Response;
 
 public class InitializationDAOImpl implements InitializationDAO {
 
 	@Override
 	public Response initDAO() {
-		// TODO Auto-generated method stub
-		return null;
+		ConnectionPool connectionPool = ConnectionPool.getInstance();
+		Response response = new Response();
+		
+		try {
+			connectionPool.init();
+			response.setMessage("database has been initialized");
+		} catch (SQLException e) {
+			response.setErrorMessage("database has been initialized");
+			response.setStatusError(true);
+			e.printStackTrace();
+		}
+		return response;
 	}
 	
 }
