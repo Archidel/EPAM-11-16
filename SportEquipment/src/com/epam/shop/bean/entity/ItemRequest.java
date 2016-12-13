e69d65c201ca35b3555ec092fd0aefe93eeac3ae
Archidel <archangel.albert.6@gmail.com>
2016-12-13 18:53:00 +0300

    Create entity item, showlist request.

--- SportEquipment/src/com/epam/shop/bean/entity/ItemRequest.java
+++ SportEquipment/src/com/epam/shop/bean/entity/ItemRequest.java
@@ -1,7 +1,16 @@
 package com.epam.shop.bean.entity;
 
 import com.epam.shop.bean.Request;
+import com.epam.shop.bean.SportEquipment;
 
 public class ItemRequest extends Request{
-	
+	private SportEquipment equipment;
+
+	public SportEquipment getEquipment() {
+		return equipment;
+	}
+
+	public void setEquipment(SportEquipment equipment) {
+		this.equipment = equipment;
+	}
 }
--- SportEquipment/src/com/epam/shop/bean/entity/ShowListRequest.java
+++ SportEquipment/src/com/epam/shop/bean/entity/ShowListRequest.java
@@ -1,7 +1,18 @@
 package com.epam.shop.bean.entity;
 
+import java.util.ArrayList;
+
 import com.epam.shop.bean.Request;
+import com.epam.shop.bean.SportEquipment;
 
 public class ShowListRequest extends Request{
-	
+	private ArrayList<SportEquipment> list;
+
+	public ArrayList<SportEquipment> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<SportEquipment> list) {
+		this.list = list;
+	}
 }
