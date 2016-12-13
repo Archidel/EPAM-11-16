5f6267be3107ca00fa59629257a109e4f247e981
Archidel <archangel.albert.6@gmail.com>
2016-12-13 18:56:25 +0300

    add lines in Response class

--- SportEquipment/src/com/epam/shop/bean/Response.java
+++ SportEquipment/src/com/epam/shop/bean/Response.java
@@ -1,5 +1,28 @@
 package com.epam.shop.bean;
 
 public class Response {
-
+	
+	private String message;
+	private String errorMessage;
+	private boolean statusError;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public boolean isStatusError() {
+		return statusError;
+	}
+	public void setStatusError(boolean statusError) {
+		this.statusError = statusError;
+	}
+	
 }
