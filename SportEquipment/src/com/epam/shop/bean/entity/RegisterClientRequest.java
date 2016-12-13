b79167fc277db529f8e0e7962e0474f917ba0cc1
Archidel <archangel.albert.6@gmail.com>
2016-12-13 18:54:37 +0300

    add RegusterClient

---
+++ SportEquipment/src/com/epam/shop/bean/entity/RegisterClientRequest.java
@@ -0,0 +1,19 @@
+package com.epam.shop.bean.entity;
+
+public class RegisterClientRequest {
+	private String nameClient;
+	private String surnameCline;
+	
+	public String getNameClient() {
+		return nameClient;
+	}
+	public void setNameClient(String nameClient) {
+		this.nameClient = nameClient;
+	}
+	public String getSurnameCline() {
+		return surnameCline;
+	}
+	public void setSurnameCline(String surnameCline) {
+		this.surnameCline = surnameCline;
+	}
+}
--- SportEquipment/src/com/epam/shop/view/View.java
+++ SportEquipment/src/com/epam/shop/view/View.java
@@ -3,5 +3,8 @@ package com.epam.shop.view;
 public class View {
 	public static void main(String [] args){
 		
+		
+		
+		
 	}
 }
