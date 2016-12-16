16008f9406560cbcdbb14b5a3812e08f58f8d940
Archidel <archangel.albert.6@gmail.com>
2016-12-16 16:18:58 +0300

    fix listResponse

--- SportEquipment/src/com/epam/store/bean/ListResponse.java
+++ SportEquipment/src/com/epam/store/bean/ListResponse.java
@@ -6,7 +6,6 @@ import com.epam.store.bean.entity.Equipment;
 
 public class ListResponse extends Response{
 	private ArrayList<Equipment> list;
-	private String type; //Лучше добавить enum (наверное)
 
 	public ArrayList<Equipment> getList() {
 		return list;
@@ -15,18 +14,9 @@ public class ListResponse extends Response{
 	public void setList(ArrayList<Equipment> list) {
 		this.list = list;
 	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
 	
 	public Equipment getElementListByIndex(int index){
 		return list.get(index);
-	}
-	
+	}	
 	
 }
